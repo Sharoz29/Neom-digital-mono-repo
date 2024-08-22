3b205d100a2433acd136bde182b8c3e070ff1728
Malik Sharoz Tariq <malik.sharoz@cloudtek.biz>
2024-08-22 14:42:19 +0500

    Changes to seperate v1 and v2 for case-type endpoint

--- libs/models/src/lib/case-types/case-types-ps.enums.ts
+++ libs/models/src/lib/case-types/case-types-ps.enums.ts
@@ -4,21 +4,24 @@
 
 export enum PSCASE_TYPES {
   // CUSTOM
-  GETCREATIONPAGE = 'caseTypes:getCreationPage',
-  GETCASETYPEACTIONS = 'caseTypes:getCaseTypeActions',
-  GETCREATIONPAGEV2 = 'caseTypes:getCreationPagev2',
-  GETCASETYPEACTIONSV2 = 'caseTypes:getCaseTypeActionsv2',
+  GETCREATIONPAGEV1 = 'caseTypes:getCreationPageV1',
+  GETCASETYPEACTIONSV1 = 'caseTypes:getCaseTypeActionsV1',
+
+  GETCREATIONPAGEV2 = 'caseTypes:getCreationPageV2',
+  GETCASETYPEACTIONSV2 = 'caseTypes:getCaseTypeActionsV2',
   // CRUD
   // Do not change the pattern below this line
-  GET = 'caseTypes:get',
-  GETONE = 'caseTypes:getOne',
+  GETV1 = 'caseTypes:getV1',
+  GETV2 = 'caseTypes:getV2',
+
+  GETONEV1 = 'caseTypes:getOneV1',
+  GETONEV2 = 'caseTypes:getOneV2',
+
   POST = 'caseTypes:post',
   PUT = 'caseTypes:put',
   CREATE = 'caseTypes:create',
   DELETE = 'caseTypes:delete',
-  
-  GETV2 = 'caseTypes:getv2',
-  GETONEV2 = 'caseTypes:getOnev2',
+
   POSTV2 = 'caseTypes:postv2',
   PUTV2 = 'caseTypes:putv2',
   CREATEV2 = 'caseTypes:createv2',
