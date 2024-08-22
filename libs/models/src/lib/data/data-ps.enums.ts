d7f62c7a15beb2b09000152f6ba0eaf039a2550c
Malik Sharoz Tariq <malik.sharoz@cloudtek.biz>
2024-08-22 16:06:16 +0500

    Changing the queue values

--- libs/models/src/lib/data/data-ps.enums.ts
+++ libs/models/src/lib/data/data-ps.enums.ts
@@ -4,28 +4,28 @@
 
 export enum PSDATA {
   // CUSTOM
-  GETMETADATAV1 = 'data:getMetaData',
-  GETMETADATAV2 = 'data:getMetaData',
+  GETMETADATAV1 = 'data:getMetaDataV1',
+  GETMETADATAV2 = 'data:getMetaDataV2',
 
-  GETDATAOBJECTSV1 = 'data:getDataObjects',
-  GETDATAOBJECTSV2 = 'data:getDataObjects',
+  GETDATAOBJECTSV1 = 'data:getDataObjectsV1',
+  GETDATAOBJECTSV2 = 'data:getDataObjectsV2',
 
-  GETDATAPAGESV1 = 'data:getDataPages',
-  GETDATAPAGESV2 = 'data:getDataPages',
+  GETDATAPAGESV1 = 'data:getDataPagesV1',
+  GETDATAPAGESV2 = 'data:getDataPagesV2',
 
-  GETDATAPAGEVIEWSV1 = 'data:getDataPageViews',
-  GETDATAPAGEVIEWSV2 = 'data:getDataPageViews',
+  GETDATAPAGEVIEWSV1 = 'data:getDataPageViewsV1',
+  GETDATAPAGEVIEWSV2 = 'data:getDataPageViewsV2',
 
-  GETDATAPAGEVIEWMETADATAV1 = 'data:getDataPageViewMetaData',
-  GETDATAPAGEVIEWMETADATAV2 = 'data:getDataPageViewMetaData',
+  GETDATAPAGEVIEWMETADATAV1 = 'data:getDataPageViewMetaDataV1',
+  GETDATAPAGEVIEWMETADATAV2 = 'data:getDataPageViewMetaDataV2',
 
   // CRUD
   // Do not change the pattern below this line
-  GETV1 = 'data:get',
-  GETV2 = 'data:get',
+  GETV1 = 'data:getV1',
+  GETV2 = 'data:getV2',
 
-  GETONEV1 = 'data:getOne',
-  GETONEV2 = 'data:getOne',
+  GETONEV1 = 'data:getOneV1',
+  GETONEV2 = 'data:getOneV2',
 
   POST = 'data:post',
   PUT = 'data:put',
