58519911a39600711e4ecf628856eb5079c0f704
Malik Sharoz Tariq <malik.sharoz@cloudtek.biz>
2024-08-22 16:22:50 +0500

    Changes to seperate v1 and v2 for page endpoint endpoint

--- libs/api-libs/src/lib/dx-v2/page/page-api.service.ts
+++ libs/api-libs/src/lib/dx-v2/page/page-api.service.ts
@@ -20,73 +20,63 @@ export class PageApiService extends BaseApiService<any, any, any> {
   }
 
   getChannelById(channelId: string, req: Request): Observable<any> {
-
-      return this.client.send(PSPAGE.GETCHANNEL, {
+    return this.client
+      .send(PSPAGE.GETCHANNELV2, {
         headers: req.headers,
         channelId,
-      }).pipe(
-        catchError(error => {
-          throw new HttpException(
-            error.message,
-            error?.status 
-          );
+      })
+      .pipe(
+        catchError((error) => {
+          throw new HttpException(error.message, error?.status);
         })
-      )
+      );
   }
   getDashboardById(dashboardId: string, req: Request): Observable<any> {
-
-      return this.client.send(PSPAGE.GETDASHBOARD, {
+    return this.client
+      .send(PSPAGE.GETDASHBOARDV2, {
         headers: req.headers,
         dashboardId,
-      }).pipe(
-        catchError(error => {
-          throw new HttpException(
-            error.message,
-            error?.status 
-          );
+      })
+      .pipe(
+        catchError((error) => {
+          throw new HttpException(error.message, error?.status);
         })
-      )
+      );
   }
   getInsightById(insightId: string, req: Request): Observable<any> {
-
-      return this.client.send(PSPAGE.GETINSIGHT, {
+    return this.client
+      .send(PSPAGE.GETINSIGHTV2, {
         headers: req.headers,
         insightId,
-      }).pipe(
-        catchError(error => {
-          throw new HttpException(
-            error.message,
-            error?.status 
-          );
+      })
+      .pipe(
+        catchError((error) => {
+          throw new HttpException(error.message, error?.status);
         })
-      )
+      );
   }
   getPageById(pageId: string, req: Request): Observable<any> {
-
-      return this.client.send(PSPAGE.GETONE, {
+    return this.client
+      .send(PSPAGE.GETONEV2, {
         headers: req.headers,
         pageId,
-      }).pipe(
-        catchError(error => {
-          throw new HttpException(
-            error.message,
-            error?.status 
-          );
+      })
+      .pipe(
+        catchError((error) => {
+          throw new HttpException(error.message, error?.status);
         })
-      )
+      );
   }
   getPortalById(portalId: string, req: Request): Observable<any> {
- 
-      return this.client.send(PSPAGE.GETPORTAL, {
+    return this.client
+      .send(PSPAGE.GETPORTALV2, {
         headers: req.headers,
         portalId,
-      }).pipe(
-        catchError(error => {
-          throw new HttpException(
-            error.message,
-            error?.status 
-          );
+      })
+      .pipe(
+        catchError((error) => {
+          throw new HttpException(error.message, error?.status);
         })
-      )
+      );
   }
 }
