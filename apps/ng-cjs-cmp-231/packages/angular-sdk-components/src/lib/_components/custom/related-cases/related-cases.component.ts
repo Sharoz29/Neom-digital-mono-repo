c88ce3f8cc0f03c340d502adb4e92aa0fb3c0c83
Malik Sharoz Tariq <malik.sharoz@cloudtek.biz>
2024-09-09 19:40:56 +0500

    Downloadable attachments

--- apps/ng-cjs-cmp-231/packages/angular-sdk-components/src/lib/_components/custom/related-cases/related-cases.component.ts
+++ apps/ng-cjs-cmp-231/packages/angular-sdk-components/src/lib/_components/custom/related-cases/related-cases.component.ts
@@ -53,7 +53,7 @@ export class RelatedCases {
     const date = new Date(dateString);
 
     const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-indexed
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
 
     return `${day}-${month}-${year}`;
