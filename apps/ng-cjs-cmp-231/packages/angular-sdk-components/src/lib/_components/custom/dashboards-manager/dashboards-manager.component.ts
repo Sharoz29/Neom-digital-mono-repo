dba65621962fe8578ee244d98b749896247f6c3c
Malik Sharoz Tariq <malik.sharoz@cloudtek.biz>
2024-09-16 16:30:26 +0500

    Implement dashboard manager page

--- apps/ng-cjs-cmp-231/packages/angular-sdk-components/src/lib/_components/custom/dashboards-manager/dashboards-manager.component.ts
+++ apps/ng-cjs-cmp-231/packages/angular-sdk-components/src/lib/_components/custom/dashboards-manager/dashboards-manager.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatCard, MatCardModule, MatCardTitle } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Utils } from 'packages/angular-sdk-components/src/public-api';
 
 @Component({
   selector: 'dashboards-manager',
   templateUrl: './dashboards-manager.component.html',
   styleUrls: ['./dashboards-manager.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [MatTable, MatCard, MatCardTitle, MatProgressSpinnerModule, CommonModule, MatCardModule, MatTableModule, MatIconModule]
 })
 export class DashboardsManager {
   @Input() pConn$: typeof PConnect;
@@ -15,15 +21,88 @@ export class DashboardsManager {
   titleIcon$: string;
   viewResources$: any;
   viewConfigProps$: any;
+  queryParams = {
+    select: [
+      {
+        field: 'pyClassName'
+      },
+      {
+        field: 'pxCreateDateTime'
+      },
+      {
+        field: 'pxCreateOperator'
+      },
+      {
+        field: 'pzInsKey'
+      },
+      {
+        field: 'pyLabel'
+      },
+      {
+        field: 'pxViewType'
+      },
+      {
+        field: 'pyRuleName'
+      },
+      {
+        field: 'pyVisibilityAccessGroup'
+      },
+      {
+        field: 'pxUpdateOperator'
+      },
+      {
+        field: 'pxUpdateOpName'
+      },
+      {
+        field: 'pxUpdateDateTime'
+      },
+      {
+        field: 'pyViewDescription'
+      },
+      {
+        field: 'pyVisibilityType'
+      },
+      {
+        field: 'pxCreateOperator:pyUserName'
+      },
+      {
+        field: 'pxUpdateOperator:pyUserName'
+      }
+    ],
+    sortBy: [
+      {
+        field: 'pxUpdateDateTime',
+        type: 'DESC'
+      }
+    ]
+  };
+  dataSource = new MatTableDataSource<any>([]);
+  dataLoaded: boolean = false;
+  displayedColumns: string[] = ['name', 'description', 'visibility', 'updateDate', 'updateOperator'];
 
   constructor(private utils: Utils) {}
 
   ngOnInit(): void {
     this.configProps$ = this.pConn$.resolveConfigProps(this.pConn$.getConfigProps());
-    this.pConn$.getCurrentView();
     this.viewResources$ = PCore.getViewResources().fetchViewResources(this.pConn$.getCurrentView(), this.pConn$, '');
     this.viewConfigProps$ = this.pConn$.resolveConfigProps(this.viewResources$).config;
     this.title$ = this.viewConfigProps$.title;
     this.titleIcon$ = this.utils.getSDKStaticContentUrl().concat(`assets/icons/${this.viewConfigProps$.icon}.svg`);
+    PCore.getDataPageUtils()
+      .getDataAsync('D_pzDashboardsList', 'app/primary_1', {}, '', this.queryParams)
+      .then(res => {
+        this.dataLoaded = true;
+        this.dataSource.data = res.data;
+      });
+  }
+
+  formatReadableDate(dateString: string) {
+    const date = new Date(dateString);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = date.toLocaleString('en-US', { month: 'long' });
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year}`;
   }
 }
